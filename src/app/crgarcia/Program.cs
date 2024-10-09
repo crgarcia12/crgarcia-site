f0a34fa7a144e1cd01fed978b38f89b443f2bc4c
Carlos Garcia Lalicata <crgarcia@live.com.ar>
2024-10-09 18:07:34 +0200

    atempt 1

--- src/app/crgarcia/Program.cs
+++ src/app/crgarcia/Program.cs
@@ -12,10 +12,10 @@ namespace crgarcia
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
-            if (!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            //if (!app.Environment.IsDevelopment())
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
             app.UseStaticFiles();
 
             app.UseRouting();
